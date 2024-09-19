ce6289b94e2dbb321dfbdb57f7e5869f760804b9
luotao <luotaoyeah@gmail.com>
2024-09-19 23:17:33 +0800

    feat(doc-18.3): https://react.dev/learn/javascript-in-jsx-with-curly-braces

---
+++ projects/doc-18.3/src/javascript-in-jsx-with-curly-braces/index.jsx
@@ -0,0 +1,20 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../index.css";
+
+// JSX 中可以通过花括号{}嵌入JS表达式
+function A() {
+    // 有两个地方可以用花括号:
+    //   1. 在文本中
+    //   2. 在属性名的=后面
+
+    const a = "LUOTAO";
+
+    return <div data-a={a}>NAME: {a}</div>;
+}
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <A />
+    </StrictMode>,
+);

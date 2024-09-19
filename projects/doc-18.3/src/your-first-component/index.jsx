760fde065551dd8cb6278ea92ccd5833d9aad5ea
luotao <luotaoyeah@gmail.com>
2024-09-19 22:27:15 +0800

    feat(doc-18.3): https://react.dev/learn/your-first-component

--- projects/doc-18.3/src/your-first-component/index.jsx
+++ projects/doc-18.3/src/your-first-component/index.jsx
@@ -1,10 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "../index.css";
 
-import App from "./App";
+import Profile, { A } from "./Profile";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <Profile />
+        <A></A>
     </StrictMode>,
 );

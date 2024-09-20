553d3d86e8332822dfe15b117b1073374200d10a
luotao <luotaoyeah@gmail.com>
2024-09-21 05:06:32 +0800

    feat(doc-18.3): https://react.dev/learn/keeping-components-pure

---
+++ projects/doc-18.3/src/keeping-components-pure/index.jsx
@@ -0,0 +1,15 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../index.css";
+
+function A() {
+    console.log("A()");
+    return <div></div>;
+}
+
+createRoot(document.getElementById("root")).render(
+    /* 在严格模式下, 在开发环境时, 每个组件函数在渲染的时候会执行两次, 这样可以验证该组件是否是纯函数, */
+    <StrictMode>
+        <A />
+    </StrictMode>,
+);

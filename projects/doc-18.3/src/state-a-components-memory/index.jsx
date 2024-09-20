dda0b1010fe333554066a6295615eb8a7b09fbf0
luotao <luotaoyeah@gmail.com>
2024-09-21 07:31:19 +0800

    feat(doc-18.3): https://react.dev/learn/state-a-components-memory
    useState()

--- projects/doc-18.3/src/state-a-components-memory/index.jsx
+++ projects/doc-18.3/src/state-a-components-memory/index.jsx
@@ -4,9 +4,14 @@ import "../index.css";
 
 function A() {
     // useState() 返回一个数组, 包含两个元素, 第一个元素表示状态数据, 第二个元素表示用来修改该状态数据的函数,
+    // useState() 是根据它的调用顺序来确定每次调用时对应的是哪个状态数据,
     const [index, setIndex] = useState(0);
 
+    console.log("A()", index);
+
     // hook 函数必须在组件的最顶层调用,
+
+    // 一个组件的多个实例中, 他们的 state 是相互独立的,
 
     return (
         <div>
@@ -23,6 +28,7 @@ function A() {
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
+        <A />
         <A />
     </StrictMode>,
 );

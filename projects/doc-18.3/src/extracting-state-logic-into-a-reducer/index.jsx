6481ea7ce8ac01bc6458e609249fe52d6f2bffa5
luotao <luotaoyeah@gmail.com>
2024-09-22 19:33:11 +0800

    feat(doc-18.3): https://react.dev/learn/extracting-state-logic-into-a-reducer: useImmerReducer

--- projects/doc-18.3/src/extracting-state-logic-into-a-reducer/index.jsx
+++ projects/doc-18.3/src/extracting-state-logic-into-a-reducer/index.jsx
@@ -1,6 +1,7 @@
 import React, { StrictMode, useReducer, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "../index.css";
+import { useImmerReducer } from "use-immer";
 
 function A() {
     const [count, setCount] = useState(0);
@@ -42,9 +43,40 @@ function B() {
     );
 }
 
+function countImmerReducer(draft, action) {
+    switch (action.type) {
+        case "increment": {
+            draft.value += 1;
+            break;
+        }
+
+        case "decrement": {
+            draft.value -= 1;
+            break;
+        }
+
+        default: {
+            throw new Error("NOT SUPPORTED");
+        }
+    }
+}
+
+function C() {
+    const [count, dispatch] = useImmerReducer(countImmerReducer, { value: 0 });
+
+    return (
+        <div>
+            <div>{count.value}</div>
+            <button onClick={() => dispatch({ type: "increment" })}>+</button>
+            <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+        </div>
+    );
+}
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <A />
         <B />
+        <C />
     </StrictMode>,
 );

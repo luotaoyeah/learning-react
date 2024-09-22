541bc482b926c6b504966fa588c27a5cecdaeff9
luotao <luotaoyeah@gmail.com>
2024-09-22 20:45:24 +0800

    feat(doc-18.3): https://react.dev/learn/passing-data-deeply-with-context: createContext(), useContext()

---
+++ projects/doc-18.3/src/passing-data-deeply-with-context/SomeContext.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+// 使用 createContext() 定义一个 context, 必须提供默认值,
+export const SomeContext = createContext("SOME CONTEXT DEFAULT");
---
+++ projects/doc-18.3/src/passing-data-deeply-with-context/index.jsx
@@ -0,0 +1,50 @@
+import React, { StrictMode, useContext } from "react";
+import { createRoot } from "react-dom/client";
+import "../index.css";
+import { SomeContext } from "./SomeContext";
+
+function A() {
+    // 如果上级组件中找不到对应的 context, 则会使用该 context 的默认值,
+    const some = useContext(SomeContext);
+
+    return (
+        <fieldset>
+            <legend>A</legend>
+
+            <div>{some}</div>
+
+            <SomeContext.Provider value={"SOME CONTEXT"}>
+                <B />
+            </SomeContext.Provider>
+        </fieldset>
+    );
+}
+
+function B() {
+    return (
+        <fieldset>
+            <legend>B</legend>
+
+            <C />
+        </fieldset>
+    );
+}
+
+function C() {
+    // 使用 useContext() 使用一个 context,
+    const some = useContext(SomeContext);
+
+    return (
+        <fieldset>
+            <legend>C</legend>
+
+            <div>{some}</div>
+        </fieldset>
+    );
+}
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <A />
+    </StrictMode>,
+);

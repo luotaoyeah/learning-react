de13889e60e333887c7e7fc4e60dc0b24fc018d7
luotao <luotaoyeah@gmail.com>
2024-09-24 20:56:34 +0800

    feat(doc-18.3): https://react.dev/learn/synchronizing-with-effects: useEffect(): no dependencies

--- projects/doc-18.3/src/synchronizing-with-effects/index.jsx
+++ projects/doc-18.3/src/synchronizing-with-effects/index.jsx
@@ -1,4 +1,4 @@
-import React, { StrictMode, useEffect, useState } from "react";
+import React, { StrictMode, useEffect, useRef, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "../index.css";
 
@@ -6,7 +6,7 @@ function A() {
     const [count, setCount] = useState(0);
 
     useEffect(() => {
-        console.log("EFFECT");
+        console.log("A | useEffect()");
     });
 
     return (
@@ -27,8 +27,48 @@ function A() {
     );
 }
 
+function B() {
+    const [isPlay, setIsPlay] = useState(false);
+    const [text, setText] = useState("");
+    const videoRef = useRef(null);
+
+    // 当没有指定 dependencies 时, 每次 render 之后都会触发,
+    useEffect(() => {
+        console.log("B | useEffect()");
+        if (isPlay) {
+            videoRef.current.play();
+        } else {
+            videoRef.current.pause();
+        }
+    });
+
+    return (
+        <fieldset>
+            <legend>B</legend>
+
+            <div>
+                <video autoPlay={false} ref={videoRef} src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"></video>
+            </div>
+            <div>
+                <button
+                    onClick={() => {
+                        setIsPlay(!isPlay);
+                    }}
+                >
+                    {isPlay ? "PAUSE" : "PLAY"}
+                </button>
+            </div>
+
+            <div>
+                <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+            </div>
+        </fieldset>
+    );
+}
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <A />
+        <B />
     </StrictMode>,
 );

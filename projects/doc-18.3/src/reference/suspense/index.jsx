c08cfdda2f724aa090479cf2d2825cb86e2ca037
luotao <luotaoyeah@gmail.com>
2024-09-26 22:50:21 +0800

    feat(doc-18.3): https://react.dev/reference/react/Suspense#revealing-content-together-at-once

--- projects/doc-18.3/src/reference/suspense/index.jsx
+++ projects/doc-18.3/src/reference/suspense/index.jsx
@@ -3,12 +3,10 @@ import { lazy, StrictMode, Suspense } from "react";
 import "../../index.css";
 
 // https://react.dev/reference/react/Suspense#displaying-a-fallback-while-content-is-loading
-function A() {
+function Loading() {
     return (
         <fieldset>
-            <legend>A</legend>
-
-            <div>B IS LOADING, PLEASE WAIT.</div>
+            <legend>LOADING, PLEASE WAIT</legend>
         </fieldset>
     );
 }
@@ -17,13 +15,11 @@ function _B() {
     return (
         <fieldset>
             <legend>B</legend>
-
-            <div>B</div>
         </fieldset>
     );
 }
 
-// Use lazy() to load a component lazily,
+// 使用 lazy() 延迟加载一个组件,
 const B = lazy(
     () =>
         new Promise((resolve) => {
@@ -33,10 +29,32 @@ const B = lazy(
         }),
 );
 
+// https://react.dev/reference/react/Suspense#revealing-content-together-at-once
+function C({ children }) {
+    return (
+        <fieldset>
+            <legend>C</legend>
+
+            {children}
+        </fieldset>
+    );
+}
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <Suspense fallback={<A />}>
+        <Suspense fallback={<Loading />}>
             <B />
+        </Suspense>
+
+        <hr />
+
+        <div>// Suspense 下面只要有一个正在加载的组件, 即使它嵌套的很深, 则该 Suspnese 也会显示 fallback 内容, 直到所有的子孙组件都加载完成,</div>
+        <Suspense fallback={<Loading />}>
+            <C />
+            <C>
+                <B />
+            </C>
+            <C />
         </Suspense>
     </StrictMode>,
 );

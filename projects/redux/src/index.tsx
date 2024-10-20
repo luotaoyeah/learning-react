c66a2475773af7af5f311ff2a69a3083d4bb0459
luotao <luotaoyeah@gmail.com>
2024-10-20 10:59:49 +0800

    feat(redux): HELLO WORLD

---
+++ projects/redux/src/index.tsx
@@ -0,0 +1,5 @@
+import { StrictMode } from "react";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(<StrictMode>TODO</StrictMode>);
